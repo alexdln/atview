4e1814bdd2b6f5623e5cf7e708ac38c96a5dc49d
Vordgi <sasha2822222@gmail.com>
2026-04-19 14:51:55 +0100

    feat: implement ast transform logic

--- packages/core/src/core/providers/atview/index.ts
+++ packages/core/src/core/providers/atview/index.ts
@@ -1,11 +1,9 @@
 import { dataToAst } from "./data-to-ast";
 import { astToData } from "./ast-to-data";
 import { formatDocument } from "./format-document";
-import { processBlobs } from "./process-blobs";
 
 export const AtviewProvider = {
     dataToAst,
     astToData,
     formatDocument,
-    processBlobs,
 };
