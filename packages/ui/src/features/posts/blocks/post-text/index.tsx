f128cabe3727d03efc8e832e337d0aa2a7f50551
Vordgi <sasha2822222@gmail.com>
2026-04-21 21:46:45 +0100

    chore: fix facets parsing in posts

--- packages/ui/src/features/posts/blocks/post-text/index.tsx
+++ packages/ui/src/features/posts/blocks/post-text/index.tsx
@@ -1,13 +1,7 @@
+import { type AppBskyRichtextFacet } from "@atproto/api";
 import React, { Fragment } from "react";
 
-import { type AppBskyRichtextFacet } from "@atproto/api";
-import { isLink } from "@atproto/api/dist/client/types/app/bsky/richtext/facet";
-
-import {
-    bytePositionToCharPosition,
-    charPositionToBytePosition,
-    getFacetLink,
-} from "@src/features/posts/core/utils/rich-text";
+import { bytePositionToCharPosition, getFacetLink } from "@src/features/posts/core/utils/rich-text";
 
 import "./post-text.scss";
 
@@ -16,33 +10,11 @@ export interface PostTextProps {
     facets?: AppBskyRichtextFacet.Main[];
 }
 
-const bytesToChars = bytePositionToCharPosition;
-const charsToBytes = charPositionToBytePosition;
-
-export const PostText: React.FC<PostTextProps> = ({ text: oldText, facets: oldFacets }) => {
-    let text = oldText;
-    const facets = JSON.parse(JSON.stringify(oldFacets || [])) as AppBskyRichtextFacet.Main[];
-    facets?.forEach(({ features, index }, idx) => {
-        if (isLink(features[0])) {
-            const charsStart = bytesToChars(text, index.byteStart);
-            const charsEnd = bytesToChars(text, index.byteEnd);
-            text = text.substring(0, charsStart) + features[0].uri + text.substring(charsEnd);
-
-            const charsOrig = charsEnd - charsStart;
-            const charsDiff = features[0].uri.length - charsOrig;
-            index.byteEnd = charsToBytes(text, charsEnd + charsDiff);
-            facets!.slice(idx + 1).forEach((f) => {
-                f.index.byteStart = charsToBytes(text, bytesToChars(text, f.index.byteStart) + charsDiff);
-                f.index.byteEnd = charsToBytes(text, bytesToChars(text, f.index.byteEnd) + charsDiff);
-            });
-        }
-    });
-    if (!facets?.length) return <p className="post-text">{text}</p>;
-
+export const PostText: React.FC<PostTextProps> = ({ text, facets = [] }) => {
     const facetsSorted = [...facets]
         .sort((a, b) => a.index.byteStart - b.index.byteStart)
-        .filter((facet, index) => {
-            if (index > 0 && facets[index - 1].index.byteEnd > facet.index.byteStart) return false;
+        .filter((facet, index, self) => {
+            if (index > 0 && self[index - 1].index.byteEnd > facet.index.byteStart) return false;
             return true;
         });
 
@@ -81,7 +53,11 @@ export const PostText: React.FC<PostTextProps> = ({ text: oldText, facets: oldFa
             );
             return acc;
         },
-        [text.substring(0, bytePositionToCharPosition(text, facetsSorted[0].index.byteStart))],
+        [
+            facetsSorted.length
+                ? text.substring(0, bytePositionToCharPosition(text, facetsSorted[0].index.byteStart))
+                : text,
+        ],
     );
 
     return <p className="post-text">{result}</p>;
