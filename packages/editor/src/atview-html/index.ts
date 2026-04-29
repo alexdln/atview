2f0a81b417c1d14ec490e2a9bf98466133e25818
Vordgi <sasha2822222@gmail.com>
2026-04-29 12:35:29 +0100

    feat: get rid of editor

--- packages/editor/src/atview-html/index.ts
+++
@@ -1,3 +0,0 @@
-export { astToAtviewHtml, type AstToAtviewHtmlContext } from "./ast-to-atview-html";
-export { atviewHtmlToAst } from "./atview-html-to-ast";
-export { getDocumentAtviewHtml } from "./providers";
--- packages/editor/src/index.tsx
+++
@@ -1,2 +0,0 @@
-export * from "./blocks/editor-renderer";
-export * from "./atview-html";
